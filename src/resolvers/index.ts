01dfecf0797f40ea05ddc3948ba4301aa8650d8e
Jinho Hyeon <neverlish@gmail.com>
2020-03-22 18:08:58 +0900

    added me query

--- src/resolvers/index.ts
+++ src/resolvers/index.ts
@@ -2,6 +2,11 @@ import { prisma } from "../generated/prisma-client";
 
 export const resolvers = {
   Query: {
-    users: () => prisma.users()
+    users: () => prisma.users(),
+    me: async (_, __, { me }) => {
+      return (me && me.id)
+        ? await prisma.user({ id: me.id })
+        : null;
+    }
   },
 };
--- src/server.ts
+++ src/server.ts
@@ -2,25 +2,25 @@ require('dotenv').config();
 
 import * as cookieParser from 'cookie-parser';
 import * as fs from 'fs';
-import * as jwt from 'jsonwebtoken';
 import { GraphQLServer } from 'graphql-yoga';
+import * as jwt from 'jsonwebtoken';
 import * as path from 'path';
 import { resolvers } from './resolvers';
 import authRouter from './routes/auth';
 
-
 const typeDefs = fs.readFileSync(path.join(__dirname, './generated.graphql')).toString();
+const schema = fs.readFileSync(path.join(__dirname, './schema.graphql')).toString();
 
 const server = new GraphQLServer({
-  typeDefs: [typeDefs],
+  typeDefs: [typeDefs, schema],
   resolvers,
   resolverValidationOptions: { requireResolversForResolveType: false },
   context: (req) => {
     const authorization = (req.request.headers.authorization ? req.request.headers.authorization.replace(/^Bearer\s*?/, '') : '').trim();
-    const user = authorization
+    const me = authorization
       ? jwt.verify(authorization, process.env.JWTSECRET)
       : undefined;
-    return { req, user };
+    return { req, me };
   },
 });
 
