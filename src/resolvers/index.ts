502e9234d47f6fc973e8e178ae7fe2b2b603bc55
Jinho Hyeon <neverlish@gmail.com>
2020-03-27 01:14:43 +0900

    added resolvers

--- src/resolvers/index.ts
+++ src/resolvers/index.ts
@@ -9,10 +9,25 @@ export const resolvers = {
         : null;
     },
     books: (_, args) => prisma.books(args),
-    book: (_, args) => prisma.book(args),
+    book: (_, args) => prisma.book({ id: args.where.id }),
+  },
+  User: {
+    bookReads: ({ id }) => prisma.user({ id }).bookReads(),
+    bookWants: ({ id }) => prisma.user({ id }).bookWants(),
   },
   Book: {
     author: ({ id }) => prisma.book({ id }).author(),
     hashtags: ({ id }) => prisma.book({ id }).hashtags(),
+    bookReads: ({ id }) => prisma.book({ id }).bookReads(),
+    bookWants: ({ id }) => prisma.book({ id }).bookWants(),
+    userRegistered: ({ id }) => prisma.book({ id }).userRegistered(),
+  },
+  UserBookWant: {
+    user: ({ id }) => prisma.userBookWant({ id }).user(),
+    book: ({ id }) => prisma.userBookWant({ id }).book(),
+  },
+  UserBookRead: {
+    user: ({ id }) => prisma.userBookRead({ id }).user(),
+    book: ({ id }) => prisma.userBookRead({ id }).book(),
   },
 };
